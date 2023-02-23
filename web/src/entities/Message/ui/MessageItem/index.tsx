e6ca859a36dc6c2eccdf918a86ea48259d74c708
Nikita Rogovskiy <rogich6319@gmail.com>
2023-02-24 02:43:56 +0300

    Fixed fetching user sender

--- web/src/entities/Message/ui/MessageItem/index.tsx
+++ web/src/entities/Message/ui/MessageItem/index.tsx
@@ -1,10 +1,8 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import { DirectMessage } from "../../config/types";
 import Container from "./styles/Container";
 import { Avatar, Typography } from "@mui/material";
 import Wrapper from "./styles/Wrapper";
-import { userService } from "../../../../shared/services/UserService";
-import useAsyncEffect from "use-async-effect";
 import MessageAndInfo from "./styles/MessageAndInfo";
 import { MessagePosition } from "../../config/MessagePositions";
 import { isNull } from "lodash";
@@ -13,27 +11,20 @@ import UserNameAndTime from "./styles/UserNameAndTime";
 import { getFirstName } from "../../../User/helpers/getName";
 import SmallTimeText from "../../../../shared/ui/SmallTimeText";
 import { useUser } from "../../../User/model/hooks/user";
+import { User } from "../../../User/model/types/User";
 
 type MessageItemProps = {
   item: DirectMessage;
+  sender: User | undefined;
 };
 
-const MessageItem: FunctionComponent<MessageItemProps> = ({ item }) => {
-  const [fetchedUser, setFetchedUser] = useState<any | undefined>();
+const MessageItem: FunctionComponent<MessageItemProps> = ({ item, sender }) => {
   const user = useUser((state) => state.user);
-
-  useAsyncEffect(async () => {
-    const result = await userService.getUserByUid(item.uid);
-    if (!result) {
-      return;
-    }
-    setFetchedUser(result);
-  }, [item]);
 
   const isMe: boolean = !isNull(user) && user.uid === item.uid;
   const messagePosition = isMe ? MessagePosition.RIGHT : MessagePosition.LEFT;
 
-  if (!fetchedUser) return <></>;
+  if (!sender) return <></>;
 
   return (
     <Wrapper pos={messagePosition}>
@@ -52,11 +43,11 @@ const MessageItem: FunctionComponent<MessageItemProps> = ({ item }) => {
             {dateFormat(item.createdAt, "shortTime")}{" "}
           </SmallTimeText>
           <Typography textAlign="center">
-            {fetchedUser && getFirstName(fetchedUser.displayName)}
+            {sender && getFirstName(sender.displayName)}
           </Typography>
         </UserNameAndTime>
       </MessageAndInfo>
-      <Avatar src={fetchedUser && fetchedUser?.photoURL} />
+      <Avatar src={sender && sender?.photoURL} />
     </Wrapper>
   );
 };
